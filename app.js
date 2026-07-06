51913ade6a219421177111a2768a12b0d85a9f8c
Gayan Gamage <gayan.gamage@gmail.com>
2026-07-06 12:41:19 -0400

    first Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+res.render('form');
+});
+
+
+app.listen(3000, () => {
+console.log('Server running on http://localhost:3000');
+});
