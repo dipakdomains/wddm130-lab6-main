45b13610c941d83e992143791255529fc355e475
Gayan Gamage <gayan.gamage@gmail.com>
2026-07-06 13:20:19 -0400

    third

--- app.js
+++ app.js
@@ -4,10 +4,31 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:false}));
+
 
 app.get('/', (req, res) => {
-res.render('form');
+    res.render('form');
 });
+
+//To handle the form info
+app.post('/process', (req, res) => {
+   
+    var name = req.body.name;
+    var email = req.body.email;
+    var postal = req.body.postcode;
+    var phNo = req.body.phone;
+    var needLunch = req.body.lunch;
+    var numTickets = req.body.tickets;
+    var campus= req.body.campus;
+
+    console.log(`${needLunch} -- ${numTickets} -- ${campus}`);
+
+    res.redirect('/');
+});
+
+
+
 
 
 app.listen(3000, () => {
