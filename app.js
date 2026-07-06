2e985e448d0fc210225aba84e097c0de93252f1f
Gayan Gamage <gayan.gamage@gmail.com>
2026-07-06 14:11:40 -0400

    ee

--- app.js
+++ app.js
@@ -5,30 +5,40 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:false}));
-
+const {check, validationResult} = require('express-validator'); // ES6 standard for destructuring an object
 
 app.get('/', (req, res) => {
     res.render('form');
 });
 
 //To handle the form info
-app.post('/process', (req, res) => {
+app.post('/process',[
+    check('name', 'Must have a name').notEmpty(),
+    check('email', 'Must have email').isEmail()
+    ],
+ (req, res) => {
    
-    var name = req.body.name;
-    var email = req.body.email;
-    var postal = req.body.postcode;
-    var phNo = req.body.phone;
-    var needLunch = req.body.lunch;
-    var numTickets = req.body.tickets;
-    var campus= req.body.campus;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        //when there are errors in form
+        res.render('form', {errorInfo: errors.array()} );
+    }else{
 
-    console.log(`${needLunch} -- ${numTickets} -- ${campus}`);
+        //when no errors in form
+        var name = req.body.name;
+        var email = req.body.email;
+        var postal = req.body.postcode;
+        var phNo = req.body.phone;
+        var needLunch = req.body.lunch;
+        var numTickets = req.body.tickets;
+        var campus= req.body.campus;
 
-    res.redirect('/');
+        console.log(`${needLunch} -- ${numTickets} -- ${campus}`);
+
+        res.redirect('/');
+    }
+
 });
-
-
-
 
 
 app.listen(3000, () => {
